2e368eebcb16aa662446daaf1eb959b6fc5d4081
kamiku-0102 <137014032+kamiku-0102@users.noreply.github.com>
2023-07-31 00:30:59 +0900

    Add files via upload

--- 8/calc_a_b.m
+++ 8/calc_a_b.m
@@ -13,12 +13,14 @@ x = 0:0.1:6;
 y = a * x + b;
 
 
-
+%グラフの作成
 plot(x, y, 'LineWidth',2);
 
+%範囲の指定
 xlim([0 6]);
 ylim([0 18]);
 
+%データのプロット
 hold on;
 plot(Phi(:,2), t, '.', 'MarkerSize',15);
 hold off;
